54fc121e189d6c33a1288a4227da21519e09d621
EddyNeddine <Eddy@neddine.be>
2022-05-16 12:39:23 +0200

    Basic Wix config - needs to be checked

---
+++ SimpleApp/Helper/BindableBase.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SimpleApp.Helper
+{
+    public abstract class BindableBase : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value)) return false;
+            storage = value;
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
